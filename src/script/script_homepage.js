a0f6a97fd7242d304cdfcf7e3a0cdb4825b389b9
Pedro Henrique Diniz Amorim <pdiniz0117@gmial.com>
2026-05-21 00:44:27 -0300

    limpeza de código

--- src/script/script_homepage.js
+++ src/script/script_homepage.js
@@ -1,104 +1,59 @@
-const token =
-    localStorage.getItem("token");
-
-const usuario = JSON.parse(
-    localStorage.getItem("usuario")
-);
-
-console.log(usuario);
-
+const token = localStorage.getItem("token");
+const usuario = JSON.parse(localStorage.getItem("usuario"));
 const usuarioId = usuario.id;
-
 const msg_user = document.querySelector(".msg_user");
 
 msg_user.innerText = `Bem vindo, ${usuario.nome}`;
-
-fetch(`http://localhost:3000/contas/${usuarioId}`, {
-
-    headers: {
-
-        Authorization: `Bearer ${token}`
-
-    }
-
-})
+fetch(`http://localhost:3000/contas/${usuarioId}`, 
+    {
+        headers: 
+        {
+           Authorization: `Bearer ${token}`
+        }
+    })
 
 .then(response => response.json())
+.then(data => 
+    {
 
-.then(data => {
-
-    console.log(data);
-
-saldoReal =
-Number(data.conta.saldo)
-.toLocaleString('pt-BR', {
-
-style: 'currency',
-currency: 'BRL'
-
-});
-
-    saldo.innerText = saldoReal;
-
-})
-
-.catch(error => {
-
-    console.error("Erro:", error);
-
-});
-// ========================= SALDO =========================
-
-const saldo = document.getElementById("saldo");
-
-const mostrarSaldo =
-    document.getElementById("mostrarSaldo");
-
-let saldoReal = "";
-
-let saldoVisivel = true;
-
-
-// ========================= MOSTRAR / ESCONDER SALDO =========================
-
-mostrarSaldo.addEventListener("click", () => {
-
-    if(saldoVisivel){
-
-        saldo.innerText = "R$ --,--";
-
-        mostrarSaldo.src =
-        "/src/assets/icone_olho_fechado.png";
-
-        saldoVisivel = false;
-
-    } else {
+        saldoReal = Number(data.conta.saldo).toLocaleString
+        ('pt-BR', 
+            {
+            style: 'currency',
+            currency: 'BRL'
+            }
+        );
 
         saldo.innerText = saldoReal;
+    })
 
-        mostrarSaldo.src =
-        "/src/assets/icone_olho_aberto.png";
+.catch(error => 
+    {
 
-        saldoVisivel = true;
-    }
+        console.error("Erro:", error);
 
-});
+    });
 
+const saldo = document.getElementById("saldo");
+const mostrarSaldo = document.getElementById("mostrarSaldo");
 
-// ========================= PEGAR DADOS DO USUÁRIO =========================
+let saldoReal = "";
+let saldoVisivel = true;
 
+mostrarSaldo.addEventListener("click", () => 
+    {
 
+        if(saldoVisivel)
+        {
+            saldo.innerText = "R$ ••••••";
+            mostrarSaldo.src = "/src/assets/icone_olho_fechado.png";
+            saldoVisivel = false;
+        } 
 
-
-// ========================= BUSCAR SALDO NA API =========================
-
-
-
-    
-    // ========================= FORMATAR SALDO =========================
-
-
-
-
-    // ========================= COLOCAR NA TELA =========================
-
+        else 
+        {
+            saldo.innerText = saldoReal;
+            mostrarSaldo.src = "/src/assets/icone_olho_aberto.png";
+            saldoVisivel = true;
+        }
+    });
